7ae0c95aa9014b37d2a500966f994998f0dfee8a
Ark21bit <90682332+Ark21bit@users.noreply.github.com>
2022-10-14 12:28:42 +0000

    готово

--- scripts/castom.js
+++ scripts/castom.js
@@ -1,3 +1,20 @@
-/* $(".video").click(function (e) {     
-    $(this).play();    
-}); */
+
+$(".overlay").click(function (e) {     
+    $(".modal").hide();    
+    $(".overlay").hide();    
+});
+
+$(".close_modal").click(function (e) {     
+    $(".close_modal").parent().hide();    
+    $(".overlay").hide();    
+});
+
+$("#modal2").click(function (e) {     
+    $("#autorization").show();    
+    $(".overlay").show();    
+});
+
+$("#modal1").click(function (e) {     
+    $("#reg").show();    
+    $(".overlay").show();    
+});
